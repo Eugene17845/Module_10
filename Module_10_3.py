e3aeeb86b4f79dbbf2956e09ba42da5187429a54
Eugene17845 <razdolbai55h@gmail.com>
2024-11-27 21:03:53 +0300

    Module_10_3.py

--- Module_10_3.py
+++ Module_10_3.py
@@ -11,7 +11,9 @@ class Bank(Thread):
         self.lock = Lock()
 
     def deposit(self):
-        for i in range(100):
+        transaction = 100
+        while transaction >= 0:
+            transaction -= 1
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
             rand = random.randint(50,500)
@@ -20,7 +22,9 @@ class Bank(Thread):
             print(f'Пополнение: {rand}. Баланс: {self.balance}')
 
     def take(self):
-        for i in range(100):
+        transaction = 100
+        while transaction >= 0:
+            transaction -= 1
             rand = random.randint(50,500)
             print(f'Запрос на {rand}')
             if rand <= self.balance:
